97ae1a61ebba0bbfb56ef1dacfc840b96d39498a
yujeong <b.imnuj@gmail.com>
2022-09-07 20:49:11 +0900

    feat: API 예외 처리 컨트롤러 생성

---
+++ src/main/java/shop/tryeat/exception/ExceptionControllerAdvice.java
@@ -0,0 +1,29 @@
+package shop.tryeat.exception;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+import java.util.Date;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import shop.tryeat.domain.item.exception.ItemNotFoundException;
+
+@RestControllerAdvice(annotations = RestController.class)
+public class ExceptionControllerAdvice {
+
+    @ResponseStatus(INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public final ExceptionResponse handleAllException(Exception exception, WebRequest request) {
+        return new ExceptionResponse(new Date(), exception.getMessage(), request.getDescription(false));
+    }
+
+    @ResponseStatus(NOT_FOUND)
+    @ExceptionHandler(ItemNotFoundException.class)
+    public final ExceptionResponse handleNotFoundException(Exception exception, WebRequest request) {
+        return new ExceptionResponse(new Date(), exception.getMessage(), request.getDescription(false));
+    }
+
+}
