154c106ed0b0ce0eda35bca652d16b4931c0e3f1
AnneYuJeong lim <b.imnuj@gmail.com>
2022-09-13 23:30:13 +0900

    refactor: 상품 예외 처리 리팩토링 (#10)
    * feat: 비즈니스 예외를 위한 최상위 BusinessException 예외 클래스 생성
    
    * refactor: ItemNotFoundException이 BusinessException을 상속받도록 변경
    
    * refactor: ExceptionHandler의 대상을 BusinessException으로 변경
    
    * refactor: ExceptionHandler의 반환 타입 변경
    
    * refactor: 로그 추가

--- src/main/java/shop/tryeat/domain/item/exception/ItemNotFoundException.java
+++ src/main/java/shop/tryeat/domain/item/exception/ItemNotFoundException.java
@@ -1,8 +1,12 @@
 package shop.tryeat.domain.item.exception;
 
-public class ItemNotFoundException extends RuntimeException {
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+import shop.tryeat.exception.BusinessException;
+
+public class ItemNotFoundException extends BusinessException {
     public ItemNotFoundException(String message) {
-        super(message);
+        super(message, NOT_FOUND);
     }
 
 }
